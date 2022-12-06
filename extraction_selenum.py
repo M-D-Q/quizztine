cb97d8b4ebbc62a02eb731b2c9cfa55c2e0a29ee
MDQ <maxdequick@gmail.com>
2022-12-06 17:04:44 +0100

    added selenium extraction for Exam Dump website

--- extraction_selenum.py
+++ extraction_selenum.py
@@ -1,4 +1,4 @@
-import pytest
+"""
 import time
 import json
 from selenium import webdriver
@@ -24,3 +24,70 @@ class TestTestcomplet(webdriver.Chrome):
     self.driver.get("https://www.briefmenow.org/comptia/which-sysv-init-configuration-file-should-be-modified-to-disable-the-ctrl-alt-delete-key-combination-2/")
     kek = self.driver.find_element(By.CSS_SELECTOR, "#post-content > div").getText()
     print(kek)
+
+kek2 = webdriver.Chrome()
+
+#kekk = TestTestcomplet()
+
+#kekk.test_testcomplet()
+
+kek2.get("https://www.briefmenow.org/comptia/which-sysv-init-configuration-file-should-be-modified-to-disable-the-ctrl-alt-delete-key-combination-2/")
+kek = self.driver.find_element(By.CSS_SELECTOR, "#post-content > div").getText()
+print(kek)"""
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
+from selenium.webdriver import ActionChains
+from selenium.webdriver import *
+from selenium.webdriver.support.ui import Select
+import time
+from datetime import date
+from selenium.webdriver.chrome.options import Options
+
+xpath1= "/html/body/div/div/section/div[2]/article/header/h1"
+xpath2 = "/html/body/div/div/section/div[2]/article/div/p[2]"
+xpath3 = "/html/body/div/div/section/div[2]/article/div/p[3]"
+xpath4 = "/html/body/div/div/section/div[2]/article/div/p[4]"
+xpath5 = "/html/body/div/div/section/div[2]/article/div/p[5]"
+
+
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+
+browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+browser.maximize_window()
+
+browser.get("https://www.briefmenow.org/comptia/which-sysv-init-configuration-file-should-be-modified-to-disable-the-ctrl-alt-delete-key-combination-2/")
+
+"""elements =  browser.find_element(By.XPATH, value=xpath2)
+data = elements.get_attribute('innerHTML')
+print(data)"""
+
+nbquestion = 1
+xpathquestion = print(f"/html/body/div/div/aside/div[2]/div/ul/li[{nbquestion}]")
+browser.find_element(By.XPATH, value=xpathquestion).click
+
+kek1 = browser.find_element(By.XPATH, value=xpath1).text
+print(kek1)
+kek2 = browser.find_element(By.XPATH, value=xpath2).get_attribute('innerHTML')
+print(kek2)
+kek3 = browser.find_element(By.XPATH, value=xpath3).get_attribute('innerHTML')
+print(kek3)
+kek4 = browser.find_element(By.XPATH, value=xpath4).get_attribute('innerHTML')
+print(kek4)
+kek5 = str(browser.find_element(By.XPATH, value=xpath5).get_attribute('innerHTML'))
+print(kek5)
+
+
+
+
+
+
+
