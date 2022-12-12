ad8b0d281d7e32afd6ed89df58e261d051fcd387
MDQ <maxdequick@gmail.com>
2022-12-12 17:01:08 +0100

    added big questionnaires support

--- auto_christine.py
+++ auto_christine.py
@@ -11,60 +11,101 @@ with open ("auto_christine.json", "r", encoding='utf-8') as dico:
     dataset = dico.read()
     data = json.loads(dataset)
 
-liste_questionnaires = ["Assessment Test", "Chapter 1: Exploring Linux Command-Line Tools", "Chapter 2: Managing Software", "Chapter 3: Configuring Hardware", "Chapter 4: Managing Files",
+liste_questionnaires_book = ["Assessment Test", "Chapter 1: Exploring Linux Command-Line Tools", "Chapter 2: Managing Software", "Chapter 3: Configuring Hardware", "Chapter 4: Managing Files",
 "Chapter 5: Booting Linux and Editing Files", "Chapter 6: Configuring the X Window System, Localization, and Printing", "Chapter 7: Administering the System",
 "Chapter 8: Configuring Basic Networking", "Chapter 9: Writing Scripts, Configuring Email, and Using Databases", "Chapter 10: Securing Your System" ]
+
+liste_questionnaires_dumps = ["LX0-103(v.1)", "101-400(v.2)", "101-350"]
 
 def show_it(liste_quest):
     for x in range(0, len(liste_quest)):
         print(f"{x} - {liste_quest[x]}")
 
+choix = str(input("""
+What do you want to study ?
+0 - Assessment Test & Chapters w/ explanations from LPIC-1 Study Guide book ?
+1 - Dumps questionnaires - Choice answers
+2 - Dumps questionnaires free input
+
+Enter chosen number here ->"""))
+
+if choix == "0" :
+    print("What Chapter  do you want to study :")
+    show_it(liste_questionnaires_book)
+    choix = str(input("Enter chosen number here -> "))
+    score = 0
+    i = 0
+    for john in data[liste_questionnaires_book[int(choix)]]:
+        print(f"Question #{john['id']}/{len(data[liste_questionnaires_book[int(choix)]])}")
+        print(john['question'])
+        response = str(input("Your answer is (only capital letters, space after commas) :"))
+        if response != john['answer']:
+            print(f""" 
+            
+    Incorrect, the answer was {john['answer']} ! 
+            
+    Here are some explanations : 
+            
+    {john['explanation']} """)
+        else : 
+            print(f"""Well done ! {response} is correct ! 
+
+    =============================================== Explanations : 
+
+    {john['explanation']} """)
+            score += 1
+        i += 1
+        print(f"Current score : {int((score/i)*100)}%")
+        osef = input("Press enter to continue...")
+        os.system('cls' if os.name == 'nt' else 'clear')
 
 
-print("What do you want to study :")
-show_it(liste_questionnaires)
-choix = str(input("Enter chosen number here -> "))
+elif choix == "1":
+    print("What dump do you want to study :")
+    show_it(liste_questionnaires_dumps)
+    choix = str(input("Enter chosen number here -> "))
+    score = 0
+    i = 0
+    for john in data[liste_questionnaires_dumps[int(choix)]]:
+        print(f"Question #{john['id']}/{len(data[liste_questionnaires_dumps[int(choix)]])}")
+        print(john['question'])
+        response = str(input("""
+Your answer is (no spaces, only capital letters) :
+--> """))
+        if response != john['answer']:
+            print(f"""
+Incorrect, the answer was {john['answer']} !
+             """)
+            try : 
+                print(f"""
 
+Fiabilité de la réponse : {john['warning']}
+URL : {john['url']})
+            """)
+            except : 
+                pass
+        else : 
+            print(f"""
+Well done ! {response} is correct ! 
+            """)
+            try : 
+                print(f"""
+                
+Fiabilité de la réponse : {john['warning']}
+URL : {john['url']})
+            """)
+            except : 
+                pass
+            score += 1
+        i += 1
+        print(f"Current score : {int((score/i)*100)}%")
+        osef = input("Press enter to continue...")
+        os.system('cls' if os.name == 'nt' else 'clear')
 
-score = 0
-i = 0
-for john in data[liste_questionnaires[int(choix)]]:
-    print(f"Question #{john['id']}/{len(data[liste_questionnaires[int(choix)]])}")
-    print(john['question'])
-    response = str(input("Your answer is (only capital letters, space after commas) :"))
-    if response != john['answer']:
-        print(f""" 
-        
-Incorrect, the answer was {john['answer']} ! 
-        
-Here are some explanations : 
-        
-{john['explanation']} """)
-    else : 
-        print(f"""Well done ! {response} is correct ! 
- =============================================== Explanations : 
-{john['explanation']} """)
-        score += 1
-    i += 1
-    print(f"Current score : {int((score/i)*100)}%")
-    osef = input("Press enter to continue...")
-    os.system('cls' if os.name == 'nt' else 'clear')
 #elif choix == "2000":
 #   score = 0
  #   i = 0
-  #  for john in data['questionnaire2018']:
-   #     print(f"Question #{john['id']}/{len(data['questionnaire2018'])}")
-    #    print(john['question'])
-     #   response = str(input("Your answer is (no spaces, only capital letters. :"))
-      #  if response != john['answer']:
-       #     print(f" Incorrect, the answer was {john['answer']} !")
-        #else : 
-         #   print(f"""Well done ! {response} is correct ! """)
-          #  score += 1
-        #i += 1
-       # print(f"Current score : {int((score/i)*100)}%")
-       # osef = input("Press enter to continue...")
-       # os.system('cls' if os.name == 'nt' else 'clear')
+  #  
 #elif choix == "3000":
   #  score = 0
    # i = 0
