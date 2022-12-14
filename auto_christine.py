eeb3d4a9644ab6142de2e699018a13a18141bb1c
MDQ <maxdequick@gmail.com>
2022-12-14 15:25:51 +0100

    various improvements

--- auto_christine.py
+++ auto_christine.py
@@ -12,10 +12,10 @@ with open ("auto_christine.json", "r", encoding='utf-8') as dico:
     data = json.loads(dataset)
 
 liste_questionnaires_book = ["Assessment Test", "Chapter 1: Exploring Linux Command-Line Tools", "Chapter 2: Managing Software", "Chapter 3: Configuring Hardware", "Chapter 4: Managing Files",
-"Chapter 5: Booting Linux and Editing Files", "Chapter 6: Configuring the X Window System, Localization, and Printing", "Chapter 7: Administering the System",
-"Chapter 8: Configuring Basic Networking", "Chapter 9: Writing Scripts, Configuring Email, and Using Databases", "Chapter 10: Securing Your System" ]
+"Chapter 5: Booting Linux and Editing Files", "Chapter 6: Configuring the X Window System, Localization, and Printing", "Chapter 7: Administering the System"]
+# WORKING ON IT "Chapter 8: Configuring Basic Networking", "Chapter 9: Writing Scripts, Configuring Email, and Using Databases", "Chapter 10: Securing Your System"
 
-liste_questionnaires_dumps = ["LX0-103(v.1)", "101-400(v.2)", "101-350"]
+liste_questionnaires_dumps = ["LX0-103(v.1)", "101-400(v.2)", "101-350", "Exam-Topics 101 - Paywall breached"]
 
 def show_it(liste_quest):
     for x in range(0, len(liste_quest)):
@@ -37,7 +37,7 @@ if choix == "0" :
     for john in data[liste_questionnaires_book[int(choix)]]:
         print(f"Question #{john['id']}/{len(data[liste_questionnaires_book[int(choix)]])}")
         print(john['question'])
-        response = str(input("Your answer is (only capital letters, space after commas) :"))
+        response = str(input("Your answer is (only capital letters, no commas, no spaces) :"))
         if response != john['answer']:
             print(f""" 
             
@@ -69,7 +69,7 @@ elif choix == "1":
         print(f"Question #{john['id']}/{len(data[liste_questionnaires_dumps[int(choix)]])}")
         print(john['question'])
         response = str(input("""
-Your answer is (no spaces, only capital letters) :
+Your answer is (no spaces, no commas, only capital letters) :
 --> """))
         if response != john['answer']:
             print(f"""
